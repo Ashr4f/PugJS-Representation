2839722fdc621ba9dff4520780ec6ef12c911345
Ashraf <ashraf@post.com>
2019-09-29 13:44:27 +0200

    Added slide functions, prev,next etc... (js)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 (() => {
     // FUNCTIONS TO USE LATER
@@ -22,4 +23,90 @@
             );
         }
     };
+
+    const COUNTER = document.querySelector("#counter");
+    const SLIDERS = document.querySelectorAll(".sliders-container .slider");
+    const SLIDER_PAGINATE_BUTTONS = document.querySelectorAll(".slider-button");
+
+    let currentSlide = SLIDERS.length - 1;
+
+    addClass(SLIDERS[currentSlide], "current");
+
+    const previousSlide = () => {
+        currentSlide--;
+
+        if (currentSlide < 0) {
+            currentSlide = SLIDERS.length - 1;
+        }
+
+        const previousItem = () => {
+            if (currentSlide <= 0) {
+                return SLIDERS.length - 1;
+            }
+
+            if (currentSlide + 1 > SLIDERS.length - 1) {
+                return 0;
+            }
+
+            return currentSlide + 1;
+        };
+
+        if (SLIDERS.length > 1) {
+            addClass(SLIDERS[previousItem()], "previous");
+            addClass(SLIDERS[previousItem()], "backward");
+            if (previousItem() === SLIDERS.length - 1 && currentSlide === 0) {
+                addClass(SLIDERS[1], "previous");
+                addClass(SLIDERS[1], "backward");
+            }
+            addClass(SLIDERS[currentSlide], "backward");
+            addClass(SLIDERS[currentSlide], "current");
+        }
+    };
+
+    const nextSlide = () => {
+        currentSlide++;
+
+        if (currentSlide >= SLIDERS.length) {
+            currentSlide = 0;
+        }
+
+        const previousItem =
+            currentSlide <= 0 ? SLIDERS.length - 1 : currentSlide - 1;
+
+        if (SLIDERS.length > 1) {
+            addClass(SLIDERS[previousItem], "previous");
+            addClass(SLIDERS[currentSlide], "current");
+        }
+    };
+
+    document.addEventListener("keydown", ev => {
+        if (ev.keyCode === 37 || ev.keyCode === 40) {
+            document.querySelector(".paginate-left").click();
+        }
+        if (ev.keyCode === 38 || ev.keyCode === 39) {
+            document.querySelector(".paginate-right").click();
+        }
+    });
+
+    SLIDER_PAGINATE_BUTTONS.forEach(button => {
+        button.addEventListener("click", el => {
+            SLIDERS.forEach(slider => {
+                removeClass(slider, "current");
+                removeClass(slider, "previous");
+                removeClass(slider, "backward");
+            });
+
+            if (el.currentTarget.classList.contains("paginate-left")) {
+                previousSlide();
+            }
+
+            if (el.currentTarget.classList.contains("paginate-right")) {
+                nextSlide();
+            }
+
+            COUNTER.innerText = `${currentSlide + 1}/${SLIDERS.length}`;
+        });
+    });
+
+    COUNTER.innerText = `${currentSlide + 1}/${SLIDERS.length}`;
 })();
